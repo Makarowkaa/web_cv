d906d0818b391414d9482d6a8308bbc74a7afef1
Vladyslav Makarov <krats1904@gmail.com>
2023-03-25 19:15:36 +0200

    Admin implemented for companies

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'company_name', 'country', 'email', 'website')
+    list_display_links = ('id', 'company_name')
+    search_fields = ('company_name', 'country', 'email', 'website')
+    list_per_page = 25
+
+
+admin.site.register(Company, CompanyAdmin)
