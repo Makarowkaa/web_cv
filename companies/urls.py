0209f952a8333a1fe177c9a4898c5dfd11ae0424
Vladyslav Makarov <krats1904@gmail.com>
2023-03-26 13:37:34 +0300

    Urls implemented

---
+++ companies/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CompanyView, CompanyListView, CompanyDetailsView
+
+urlpatterns = [
+    path('', CompanyView.as_view(), name='companies'),
+    path('all/', CompanyListView.as_view(), name='all-companies'),
+    path('details/<int:pk>/', CompanyDetailsView.as_view(), name='company-details'),
+]
