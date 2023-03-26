befcb1cda6ae84e5fa14bcfe0b114cd48473ed4d
Vladyslav Makarov <krats1904@gmail.com>
2023-03-26 20:09:36 +0300

    Added LOGGING to settings

--- web_cv/settings.py
+++ web_cv/settings.py
@@ -146,3 +146,22 @@ JAZZMIN_SETTINGS = {
     'site_header': 'Web CV',
     'welcome_sign': 'Welcome to Web CV admin panel',
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
