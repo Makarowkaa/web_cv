17e46315b05c0de81146617a58d2a2d35c39acab
Vladyslav Makarov <krats1904@gmail.com>
2023-03-26 13:31:03 +0300

    Views for companies implemented

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .forms import CompanySelectionForm
+from .models import Company
 
-# Create your views here.
+
+class CompanyView(View):
+    template_name = 'companies/companies.html'
+    form_class = CompanySelectionForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        companies = []
+
+        context = {
+            'form': form,
+            'companies': companies
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        companies = []
+
+        if form.is_valid():
+            companies = form.filter_companies()
+
+        context = {
+            'form': form,
+            'companies': companies
+        }
+
+        return render(request, self.template_name, context)
+
+
+class CompanyListView(View):
+    template_name = 'companies/all_companies.html'
+
+    def get(self, request, *args, **kwargs):
+        companies = Company.objects.all()
+
+        context = {
+            'companies': companies
+        }
+
+        return render(request, self.template_name, context)
+
+
+class CompanyDetailsView(View):
+    template_name = 'companies/company_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        company = get_object_or_404(Company, pk=kwargs['pk'])
+
+        context = {
+            'company': company
+        }
+
+        return render(request, self.template_name, context)
