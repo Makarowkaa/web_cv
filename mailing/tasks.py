c138d7866cf8b029f0531612832ce8865fda0cdd
Vladyslav Makarov <krats1904@gmail.com>
2023-04-06 16:23:46 +0300

    Reverted celery import in tasks

--- mailing/tasks.py
+++ mailing/tasks.py
@@ -1,6 +1,7 @@
-from celery import shared_task
 from django.conf import settings
 from django.core.mail import EmailMessage
+
+from celery import shared_task
 
 from companies.models import Company
 
