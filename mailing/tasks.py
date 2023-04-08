1ebd6b0c968bc486aab76237b0bb2787c6b4c119
Vladyslav Makarov <krats1904@gmail.com>
2023-04-08 13:09:55 +0300

    cv read moved outside the for loop due to not properly handling file contents

--- mailing/tasks.py
+++ mailing/tasks.py
@@ -16,10 +16,11 @@ def send_email_task(self, subject, cover_letter, cv, selected_company_ids):
         cover_letter (str): The cover letter
         cv (File): The CV
         selected_company_ids (list): A list of selected company ids
-
     """
 
     companies = Company.objects.filter(id__in=selected_company_ids)
+
+    cv_content = cv.read()
 
     for company in companies:
         email = EmailMessage(
@@ -30,7 +31,6 @@ def send_email_task(self, subject, cover_letter, cv, selected_company_ids):
             reply_to=[settings.EMAIL_HOST_USER],
         )
 
-        if cv:
-            email.attach(cv.name, cv.read(), cv.content_type)
+        email.attach(cv.name, cv_content, cv.content_type)
 
         email.send()
